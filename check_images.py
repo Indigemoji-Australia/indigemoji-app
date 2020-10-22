bc4cf9e1d04ce1077b730a798f51b65c7141c909
Michela Ledwidge <michela@mod.studio>
2020-10-22 22:32:31 +1100

    updated data for audio

--- check_images.py
+++ check_images.py
@@ -18,9 +18,12 @@ with open("emojis.csv", "r") as infile:
             "name": row["name"],
             "name_arrernte": row["name_arrernte"],
             "file": row["file"],
+            "audio": row['audio'],
             "data": imgdata,
         }
         out["emojis"].append(item)
-
+        
+        if row['audio'] != '' and not os.path.isfile('./assets/audio/%s' % row["audio"]):
+            print("missing %s" % row["audio"])
 with open("assets/emojis.json", "w") as outfile:
     json.dump(out, outfile, indent=2)
