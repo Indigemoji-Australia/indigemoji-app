659e2b331ec00d31482e1e7b587cfc55b7b14823
unknown <will@mod.studio>
2020-10-27 20:47:38 +1100

    Changed audio files to lowercase.

--- screens/Emoji.js
+++ screens/Emoji.js
@@ -23,6 +23,7 @@ export default class IconDetail extends React.Component {
     };
   }
 
+  // All sound filenames must be in lowercase.
   // Sound files must be in the following places:
   // Android: Save your sound clip files under the directory android/app/src/main/res/raw. Note that files in this directory must be lowercase and underscored (e.g. my_file_name.mp3) and that subdirectories are not supported by Android.
   // iOS: Open Xcode and add your sound files to the project (Right-click the project and select Add Files to [PROJECTNAME])
