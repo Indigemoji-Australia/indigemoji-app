7bc6a10f16d1d6e2a118b28c93396afe85e7ef21
unknown <will@mod.studio>
2020-10-29 12:36:47 +1100

    Share button now has same styling as play button

--- screens/Emoji.js
+++ screens/Emoji.js
@@ -141,11 +141,15 @@ export default class IconDetail extends React.Component {
                 }
                 title="share"
                 type="clear"
-                titleStyle={{ color: '#676767', fontSize: 20 }}
+                titleStyle={{ 
+                  fontFamily: 'Feather',
+                  color: '#676767', 
+                  fontSize: 20 
+                }}
                 onPress={() => this.onShare(emoji)}
               />
             </View>
-            {emoji.audio
+            {emoji.audio != undefined
               ?
               <TouchableOpacity style={styles.playButton} onPress={() => this.playSound(emoji.audio)}>
                 {this.state.audioPlaying == false
@@ -222,7 +226,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   playButtonText: {
-    fontWeight: "500",
+    fontFamily: 'Feather',
     marginRight: 12,
     fontSize: 20,
     color: '#676767',
